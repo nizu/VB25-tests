f103ba86f519122f49779c4a59204dbb6a357397
nizu <nizu@vindaloovian.(none)>
2012-01-25 20:35:17 +0100

    Merging from Upstream (manual merge fixes)

--- plugins/SettingsOutput.py
+++ plugins/SettingsOutput.py
@@ -53,15 +53,6 @@ def add_properties(rna_pointer):
 			name= "Type",
 			description= "Output image format",
 			items= (
->>>>>>>>>>>>>>>>>>>> File 1
-				('PNG',     "PNG",       "PNG"),
-				('JPG',     "JPEG",      "Jpeg"),
-				('TIFF',    "Tiff",      "Tiff"),
-				('TGA',     "TGA",       "Targa"),
-				('SGI',     "SGI",       "SGI"),
-				('EXR', "OpenEXR",   "OpenEXR"),
-				('VRIMG',   "VRayImage", "V-Ray Image format"),
->>>>>>>>>>>>>>>>>>>> File 2
 				('PNG',   "PNG",       "PNG"),
 				('JPG',   "JPEG",      "Jpeg"),
 				('TIFF',  "Tiff",      "Tiff"),
@@ -69,15 +60,6 @@ def add_properties(rna_pointer):
 				('SGI',   "SGI",       "SGI"),
 				('EXR',   "OpenEXR",   "OpenEXR"),
 				('VRIMG', "VRayImage", "V-Ray Image format"),
->>>>>>>>>>>>>>>>>>>> File 3
-				('PNG',   "PNG",       "PNG"),
-				('JPG',   "JPEG",      "Jpeg"),
-				('TIFF',  "Tiff",      "Tiff"),
-				('TGA',   "TGA",       "Targa"),
-				('SGI',   "SGI",       "SGI"),
-				('EXR',   "OpenEXR",   "OpenEXR"),
-				('VRIMG', "VRayImage", "V-Ray Image format"),
-<<<<<<<<<<<<<<<<<<<<
 			),
 			default= 'PNG'
 		)
